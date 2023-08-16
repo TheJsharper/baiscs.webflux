ff49547f32a625128c51360406dae68990bcdf07
Pedro Antonio Salomon Ocampos <pedro.salomon@gmail.com>
2023-08-16 22:38:31 +0200

    simple demo blockling and non-blocking

--- src/main/java/com/jsharper/basics/webflux/service/ReactiveMathService.java
+++ src/main/java/com/jsharper/basics/webflux/service/ReactiveMathService.java
@@ -1,9 +1,10 @@
 package com.jsharper.basics.webflux.service;
 
+import java.time.Duration;
+
 import org.springframework.stereotype.Service;
 
 import com.jsharper.basics.webflux.dto.Response;
-import com.jsharper.basics.webflux.utils.MathUtils;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,7 +17,9 @@ public class ReactiveMathService {
 	}
 
 	public Flux<Response> multiplicationTable(int input) {
-		return Flux.range(1, 10).doOnNext(i -> MathUtils.sleepCurr(1))
+		return Flux.range(1, 10)
+				//.doOnNext(i -> MathUtils.sleepCurr(1))
+				.delayElements(Duration.ofSeconds(2))
 				.doOnNext(i -> System.out.println("Reactive-Math-Service processing" + i))
 				.map(i -> new Response(i * input));
 	}
